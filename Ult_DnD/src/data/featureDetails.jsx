6a41ac3d12df30141dc25bcc4ffefb58f9dfd845
alex pohlman <apohlman53@gmail.com>
2025-06-28 02:53:06 -0400

    finished barbarian class and zealot subclass

--- Ult_DnD/src/data/featureDetails.jsx
+++ Ult_DnD/src/data/featureDetails.jsx
@@ -19,4 +19,104 @@ export const featureDetails = {
     description:
       "While you aren’t wearing any armor, your base Armor Class equals 10 plus your Dexterity and Constitution modifiers. You can use a Shield and still gain this benefit.",
   },
+  "Danger Sense": {
+    name: "Danger Sense",
+    description:
+      "You gain an uncanny sense of when things aren’t as they should be, giving you an edge when you dodge perils. You have Advantage on Dexterity saving throws unless you have the Incapacitated condition.",
+  },
+  "Reckless Attack": {
+    name: "Reckless Attack",
+    description:
+      "You can throw aside all concern for defense to attack with increased ferocity. When you make your first attack roll on your turn, you can decide to attack recklessly. Doing so gives you Advantage on attack rolls using Strength until the start of your next turn, but attack rolls against you have Advantage during that time.",
+  },
+  "Primal Knowledge": {
+    name: "Primal Knowledge",
+    description:
+      "You gain proficiency in another skill of your choice from the skill list available to Barbarians at level 1. In addition, while your Rage is active, you can channel primal power when you attempt certain tasks; whenever you make an ability check using one of the following skills, you can make it as a Strength check even if it normally uses a different ability: Acrobatics, Intimidation, Perception, Stealth, or Survival. When you use this ability, your Strength represents primal power coursing through you, honing your agility, bearing, and senses. ",
+  },
+  "Extra Attack": {
+    name: "Extra Attack",
+    description:
+      "You can attack twice instead of once whenever you take the Attack action on your turn.",
+  },
+  "Fast Movement": {
+    name: "Fast Movement",
+    description:
+      "Your speed increases by 10 feet while you aren’t wearing Heavy armor.",
+  },
+  "Feral Instinct": {
+    name: "Feral Instinct",
+    description:
+      "Your instincts are so honed that you have Advantage on Initiative rolls.",
+  },
+  "Instinctive Pounce": {
+    name: "Instinctive Pounce",
+    description:
+      "As part of the Bonus Action you take to enter your Rage, you can move up to half your Speed.",
+  },
+  "Brutal Strike": {
+    name: "Brutal Strike",
+    description:
+      "If you use Reckless Attack, you can forgo any Advantage on one Strength-based attack roll of your choice on your turn. The chosen attack roll mustn’t have Disadvantage. If the chosen attack roll hits, the target takes an extra 1d10 damage of the same type dealt by the weapon or Unarmed Strike, and you can cause one Brutal Strike effect of your choice. You have the following effect options. Forceful Blow. The target is pushed 15 feet straight away from you. You can then move up to half your Speed straight toward the target without provoking Opportunity Attacks.Hamstring Blow. The target’s Speed is reduced by 15 feet until the start of your next turn. A target can be affected by only one Hamstring Blow at a time—the most recent one.",
+  },
+  "Relentless Rage": {
+    name: "Relentless Rage",
+    description:
+      "Your Rage can keep you fighting despite grievous wounds. If you drop to 0 Hit Points while your Rage is active and don’t die outright, you can make a DC 10 Constitution saving throw. If you succeed, your Hit Points instead change to a number equal to twice your Barbarian level. Each time you use this feature after the first, the DC increases by 5. When you finish a Short or Long Rest, the DC resets to 10.",
+  },
+  "Improved Brutal Strike": {
+    name: "Improved Brutal Strike",
+    description:
+      "You have honed new ways to attack furiously. The following effects are now among your Brutal Strike options. Staggering Blow. The target has Disadvantage on the next saving throw it makes, and it can’t make Opportunity Attacks until the start of your next turn. Sundering Blow. Before the start of your next turn, the next attack roll made by another creature against the target gains a +5 bonus to the roll. An attack roll can gain only one Sundering Blow bonus.",
+  },
+  "Persistent Rage": {
+    name: "Persistent Rage",
+    description:
+      "When you roll Initiative, you can regain all expended uses of Rage. After you regain uses of Rage in this way, you can’t do so again until you finish a Long Rest. In addition, your Rage is so fierce that it now lasts for 10 minutes without you needing to do anything to extend it from round to round. Your Rage ends early if you have the Unconscious condition (not just the Incapacitated condition) or don Heavy armor.",
+  },
+  "Improved Brutal Strike 2": {
+    name: "Improved Brutal Strike 2",
+    description:
+      "The extra damage of your Brutal Strike increases to 2d10. In addition, you can use two different Brutal Strike effects whenever you use your Brutal Strike feature.",
+  },
+  "Indomitable Might": {
+    name: "Indomitable Might",
+    description:
+      "If your total for a Strength check or Strength saving throw is less than your Strength score, you can use that score in place of the total.",
+  },
+  "Epic Boon": {
+    name: "Epic Boon",
+    description:
+      "You gain an Epic Boon feat or another feat of your choice for which you qualify.",
+  },
+  "Primal Champion": {
+    name: "Primal Champion",
+    description:
+      "You embody primal power. Your Strength and Constitution scores increase by 4, and their maximum is now 25.",
+  },
+  "Divine Fury": {
+    name: "Divine Fury",
+    description:
+      "You can channel divine power into your strikes. On each of your turns while your Rage is active, the first creature you hit with a weapon or an Unarmed Strike takes extra damage equal to 1d6 plus half your Barbarian level (round down). The extra damage is Necrotic or Radiant; you choose the type each time you deal the damage.",
+  },
+  "Warrior of the Gods": {
+    name: "Warrior of the Gods",
+    description:
+      "A divine entity helps ensure you can continue the fight. You have a pool of four d12s that you can spend to heal yourself. As a Bonus Action, you can expend dice from the pool, roll them, and regain a number of Hit Points equal to the roll’s total. Your pool regains all expended dice when you finish a Long Rest. The pool’s maximum number of dice increases by one when you reach Barbarian levels 6 (5 dice), 12 (6 dice), and 17 (7 dice).",
+  },
+  "Fanatical Focus": {
+    name: "Fanatical Focus",
+    description:
+      "Once per active Rage, if you fail a saving throw, you can reroll it with a bonus equal to your Rage Damage bonus, and you must use the new roll.",
+  },
+  "Zealous Presence": {
+    name: "Zealous Presence",
+    description:
+      "As a Bonus Action, you unleash a battle cry infused with divine energy. Up to ten other creatures of your choice within 60 feet of you gain Advantage on attack rolls and saving throws until the start of your next turn. Once you use this feature, you can’t use it again until you finish a Long Rest unless you expend a use of your Rage (no action required) to restore your use of it.",
+  },
+  "Rage of the Gods": {
+    name: "Rage of the Gods",
+    description:
+      "When you activate your Rage, you can assume the form of a divine warrior. This form lasts for 1 minute or until you drop to 0 Hit Points. Once you use this feature, you can’t do so again until you finish a Long Rest. While in this form, you gain the benefits below. Flight. You have a Fly Speed equal to your Speed and can hover. Resistance. You have Resistance to Necrotic, Psychic, and Radiant damage. Revivification. When a creature within 30 feet of you would drop to 0 Hit Points, you can take a Reaction to expend a use of your Rage to instead change the target’s Hit Points to a number equal to your Barbarian level.",
+  },
 };
