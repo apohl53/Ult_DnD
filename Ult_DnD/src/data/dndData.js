6440f76f73aa5c51adf1d1fc940e18687b131927
alex pohlman <apohlman53@gmail.com>
2025-06-28 09:23:45 -0400

    added monk features and long death subclass

--- Ult_DnD/src/data/dndData.js
+++ Ult_DnD/src/data/dndData.js
@@ -40,6 +40,33 @@ export const dndData = {
       },
     },
   },
+  Monk: {
+    classFeatures: {
+      1: ["Martial Arts", "Unarmored Defense(m)"],
+      2: ["Monk’s Focus", "Unarmored Movement(m)", "Uncanny Metabolism"],
+      3: ["Deflect Attacks"],
+      4: ["Slow Fall"],
+      5: ["Extra Attack", "Stunning Strike"],
+      6: ["Empowered Strikes"],
+      7: ["Evasion"],
+      9: ["Acrobatic Movement"],
+      10: ["Heightened Focus", "Self-Restoration"],
+      13: ["Deflect Energy"],
+      14: ["Disciplined Survivor"],
+      15: ["Perfect Focus"],
+      18: ["Superior Defense"],
+      19: ["Epic Boon"],
+      20: ["Body and Mind"],
+    },
+    "Long Death": {
+      subclassFeatures: {
+        3: ["Touch of Death"],
+        6: ["Hour of Reaping"],
+        11: ["Mastery of Death"],
+        17: ["Touch of Long Death"],
+      },
+    },
+  },
   Rogue: {
     classFeatures: {
       1: ["Sneak Attack"],
--- Ult_DnD/src/data/featureDetails.jsx
+++ Ult_DnD/src/data/featureDetails.jsx
@@ -1,4 +1,5 @@
 export const featureDetails = {
+  // Fighter Features
   "Second Wind": {
     name: "Second Wind",
     description:
@@ -14,6 +15,7 @@ export const featureDetails = {
     description:
       "Your weapon attacks score a critical hit on a roll of 19 or 20.",
   },
+  // Barbarian Features
   Rage: {
     name: "Rage",
     description:
@@ -123,5 +125,121 @@ export const featureDetails = {
     name: "Rage of the Gods",
     description:
       "When you activate your Rage, you can assume the form of a divine warrior. This form lasts for 1 minute or until you drop to 0 Hit Points. Once you use this feature, you can’t do so again until you finish a Long Rest. While in this form, you gain the benefits below. Flight. You have a Fly Speed equal to your Speed and can hover. Resistance. You have Resistance to Necrotic, Psychic, and Radiant damage. Revivification. When a creature within 30 feet of you would drop to 0 Hit Points, you can take a Reaction to expend a use of your Rage to instead change the target’s Hit Points to a number equal to your Barbarian level.",
+  },
+  // Bard
+  // Cleric
+  // Druid
+  // Fighter
+  // Monk
+  "Martial Arts": {
+    name: "Martial Arts",
+    description:
+      "Your practice of martial arts gives you mastery of combat styles that use your Unarmed Strike and Monk weapons, which are the following: Simple Melee weapons, Martial Melee weapons that have the Light property. You gain the following benefits while you are unarmed or wielding only Monk weapons and you aren’t wearing armor or wielding a Shield. Bonus Unarmed Strike. You can make an Unarmed Strike as a Bonus Action. Martial Arts Die. You can roll 1d6 in place of the normal damage of your Unarmed Strike or Monk weapons. This die changes as you gain Monk levels, as shown in the Martial Arts column of the Monk Features table. Dexterous Attacks. You can use your Dexterity modifier instead of your Strength modifier for the attack and damage rolls of your Unarmed Strikes and Monk weapons. In addition, when you use the Grapple or Shove option of your Unarmed Strike, you can use your Dexterity modifier instead of your Strength modifier to determine the save DC.",
+  },
+  "Unarmored Defense(m)": {
+    name: "Unarmored Defense(m)",
+    description:
+      "While you aren’t wearing armor or wielding a Shield, your base Armor Class equals 10 plus your Dexterity and Wisdom modifiers.",
+  },
+  "Monk's Focus": {
+    name: "Monk's Focus",
+    description:
+      "Your focus and martial training allow you to harness a well of extraordinary energy within yourself. This energy is represented by Focus Points. Your Monk level determines the number of points you have, as shown in the Focus Points column of the Monk Features table. You can expend these points to enhance or fuel certain Monk features. You start knowing three such features: Flurry of Blows, Patient Defense, and Step of the Wind, each of which is detailed below. When you expend a Focus Point, it is unavailable until you finish a Short or Long Rest, at the end of which you regain all your expended points. Some features that use Focus Points require your target to make a saving throw. The save DC equals 8 plus your Wisdom modifier and Proficiency Bonus. Flurry of Blows. You can expend 1 Focus Point to make two Unarmed Strikes as a Bonus Action.Patient Defense. You can take the Disengage action as a Bonus Action. Alternatively, you can expend 1 Focus Point to take both the Disengage and the Dodge actions as a Bonus Action. Step of the Wind. You can take the Dash action as a Bonus Action. Alternatively, you can expend 1 Focus Point to take both the Disengage and Dash actions as a Bonus Action, and your jump distance is doubled for the turn.",
+  },
+  "Unarmored Movement(m)": {
+    name: "Unarmored Movement(m)",
+    description:
+      "Your speed increases by 10 feet while you aren’t wearing armor or wielding a Shield. This bonus increases when you reach certain Monk levels, as shown on the Monk Features table.",
+  },
+  "Uncanny Metabolism": {
+    name: "Uncanny Metabolism",
+    description:
+      "When you roll Initiative, you can regain all expended Focus Points. When you do so, roll your Martial Arts die, and regain a number of Hit Points equal to your Monk level plus the number rolled. Once you use this feature, you can’t use it again until you finish a Long Rest.",
+  },
+  "Deflect Attacks": {
+    name: "Deflect Attacks",
+    description:
+      "When an attack roll hits you and its damage includes Bludgeoning, Piercing, or Slashing damage, you can take a Reaction to reduce the attack’s total damage against you. The reduction equals 1d10 plus your Dexterity modifier and Monk level. If you reduce the damage to 0, you can expend 1 Focus Point to redirect some of the attack’s force. If you do so, choose a creature you can see within 5 feet of yourself if the attack was a melee attack or a creature you can see within 60 feet of yourself that isn’t behind Total Cover if the attack was a ranged attack. That creature must succeed on a Dexterity saving throw or take damage equal to two rolls of your Martial Arts die plus your Dexterity modifier. The damage is the same type dealt by the attack.",
+  },
+  "Slow Fall": {
+    name: "Slow Fall",
+    description:
+      "You can take a Reaction when you fall to reduce any damage you take from the fall by an amount equal to five times your Monk level.",
+  },
+  "Stunning Strike": {
+    name: "Stunning Strike",
+    description:
+      "Once per turn when you hit a creature with a Monk weapon or an Unarmed Strike, you can expend 1 Focus Point to attempt a stunning strike. The target must make a Constitution saving throw. On a failed save, the target has the Stunned condition until the start of your next turn. On a successful save, the target’s Speed is halved until the start of your next turn, and the next attack roll made against the target before then has Advantage.",
+  },
+  "Enpowered Strikes": {
+    name: "Enpowered Strikes",
+    description:
+      "Whenever you deal damage with your Unarmed Strike, it can deal your choice of Force damage or its normal damage type.",
+  },
+  Evasion: {
+    name: "Evasion",
+    description:
+      "When you’re subjected to an effect that allows you to make a Dexterity saving throw to take only half damage, you instead take no damage if you succeed on the saving throw and only half damage if you fail. You don’t benefit from this feature if you have the Incapacitated condition.",
+  },
+  "Acrobatic Movement": {
+    name: "Acrobatic Movement",
+    description:
+      "While you aren’t wearing armor or wielding a Shield, you gain the ability to move along vertical surfaces and across liquids on your turn without falling during the movement.",
+  },
+  "Heightened Focus": {
+    name: "Heightened Focus",
+    description:
+      "Your Flurry of Blows, Patient Defense, and Step of the Wind gain the following benefits. Flurry of Blows. You can expend 1 Focus Point to use Flurry of Blows and make three Unarmed Strikes with it instead of two. Patient Defense. When you expend a Focus Point to use Patient Defense, you gain a number of Temporary Hit Points equal to two rolls of your Martial Arts die. Step of the Wind. When you expend a Focus Point to use Step of the Wind, you can choose a willing creature within 5 feet of yourself that is Large or smaller. You move the creature with you until the end of your turn. The creature’s movement doesn’t provoke Opportunity Attacks.",
+  },
+  "Self-Restoration": {
+    name: "Self-Restoration",
+    description:
+      "Through sheer force of will, you can remove one of the following conditions from yourself at the end of each of your turns: Charmed, Frightened, or Poisoned. In addition, forgoing food and drink doesn’t give you levels of Exhaustion.",
+  },
+  "Deflect Energy": {
+    name: "Deflect Energy",
+    description:
+      "You can now use your Deflect Attacks feature against attacks that deal any damage type, not just Bludgeoning, Piercing, or Slashing.",
+  },
+  "Disciplined Survivor": {
+    name: "Disciplined Survivor",
+    description:
+      "Your physical and mental discipline grant you proficiency in all saving throws. Additionally, whenever you make a saving throw and fail, you can expend 1 Focus Point to reroll it, and you must use the new roll.",
+  },
+  "Perfect Focus": {
+    name: "Perfect Focus",
+    description:
+      "When you roll Initiative and don’t use Uncanny Metabolism, you regain expended Focus Points until you have 4 if you have 3 or fewer.",
+  },
+  "Superior Defense": {
+    name: "Superior Defense",
+    description:
+      "At the start of your turn, you can expend 3 Focus Points to bolster yourself against harm for 1 minute or until you have the Incapacitated condition. During that time, you have Resistance to all damage except Force damage.",
+  },
+  "Body and Mind": {
+    name: "Body and Mind",
+    description:
+      "You have developed your body and mind to new heights. Your Dexterity and Wisdom scores increase by 4, to a maximum of 25.",
+  },
+  // Way of Long Death
+  "Touch of Death": {
+    name: "Touch of Death",
+    description:
+      "Starting when you choose this tradition at 3rd level, your study of death allows you to extract vitality from another creature as it nears its demise. When you reduce a creature within 5 feet of you to 0 hit points, you gain temporary hit points equal to your Wisdom modifier + your monk level (minimum of 1 temporary hit point).",
+  },
+  "Hour of Reaping": {
+    name: "Hour of Reaping",
+    description:
+      "At 6th level, you gain the ability to unsettle or terrify those around you as an action, for your soul has been touched by the shadow of death. When you take this action, each creature within 30 feet of you that can see you must succeed on a Wisdom saving throw or be frightened of you until the end of your next turn.",
+  },
+  "Mastery of Death": {
+    name: "Mastery of Death",
+    description:
+      "Beginning at 11th level, you use your familiarity with death to escape its grasp. When you are reduced to 0 hit points, you can expend 1 Focus point (no action required) to have 1 hit point instead.",
+  },
+  "Touch of the Long Death": {
+    name: "Touch of the Long Death",
+    description:
+      "Starting at 17th level, your touch can channel the energy of death into a creature. As an action, you touch one creature within 5 feet of you, and you expend 1 to 10 Focus points. The target must make a Constitution saving throw, and it takes 2d10 necrotic damage per Focus point spent on a failed save, or half as much damage on a successful one.",
   },
 };
