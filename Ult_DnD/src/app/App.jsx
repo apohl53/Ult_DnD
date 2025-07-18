2e4c4841ec06c101fc7b6f6b273ece01e23b6662
alex pohlman <apohlman53@gmail.com>
2025-07-17 21:44:44 -0400

    fixed hash router

--- Ult_DnD/src/app/App.jsx
+++ Ult_DnD/src/app/App.jsx
@@ -1,25 +1,27 @@
 import "./App.css";
-import { Routes, Route, Link } from "react-router-dom";
+import { HashRouter, Routes, Route, Link } from "react-router-dom";
 import CharacterPage from "./Pages/Character";
 import General from "./Pages/GeneralInfo";
 
 function App() {
   return (
-    <div className="App">
-      {/* Navigation Bar */}
-      <nav style={{ padding: "1rem", backgroundColor: "#f0f0f0" }}>
-        <Link to="/" style={{ marginRight: "1rem" }}>
-          Home
-        </Link>
-        <Link to="/general">General Info</Link>
-      </nav>
+    <HashRouter>
+      <div className="App">
+        {/* Navigation Bar */}
+        <nav style={{ padding: "1rem", backgroundColor: "#f0f0f0" }}>
+          <Link to="/" style={{ marginRight: "1rem" }}>
+            Home
+          </Link>
+          <Link to="/general">General Info</Link>
+        </nav>
 
-      {/* Routes for different pages */}
-      <Routes>
-        <Route path="/" element={<CharacterPage />} />
-        <Route path="/general" element={<General />} />
-      </Routes>
-    </div>
+        {/* Routes for different pages */}
+        <Routes>
+          <Route path="/" element={<CharacterPage />} />
+          <Route path="/general" element={<General />} />
+        </Routes>
+      </div>
+    </HashRouter>
   );
 }
 
